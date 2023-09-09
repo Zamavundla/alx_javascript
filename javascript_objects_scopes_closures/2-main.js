330043a3e320051f6b27f132648b7e2b525bc88e
Zamavundla <zsdouzee8@gmail.com>
2023-09-09 20:11:44 +0200

    .

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,23 +1,21 @@
+#!/ usr/bin/node
 const Rectangle = require('./2-rectangle');
 
 // Function to check if a rectangle is valid and print its width
+function printRectangle(rect) {
+  console.log(rect);
+  console.log(rect.width);
+  console.log(rect.height);
+}
 
 const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+printRectangle(r1);
 
 const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
+printRectangle(r2);
 
 const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+printRectangle(r3);
 
 const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
+printRectangle(r4);
