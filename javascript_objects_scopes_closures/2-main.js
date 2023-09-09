1a6a9cfbdc63b631d77bca3e016834a6ed93f33a
Zamavundla <zsdouzee8@gmail.com>
2023-09-09 20:12:55 +0200

    .

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,21 +1,42 @@
 #!/ usr/bin/node
 const Rectangle = require('./2-rectangle');
 
-// Function to check if a rectangle is valid and print its width
-function printRectangle(rect) {
-  console.log(rect);
-  console.log(rect.width);
-  console.log(rect.height);
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+
+if (r1.width !== undefined && r1.height !== undefined) {
+  console.log(r1.width);
+  console.log(r1.height);
+} else {
+  console.log('undefined');
 }
 
-const r1 = new Rectangle(2, 3);
-printRectangle(r1);
-
 const r2 = new Rectangle(2, -3);
-printRectangle(r2);
+console.log(r2);
+
+if (r2.width !== undefined && r2.height !== undefined) {
+  console.log(r2.width);
+  console.log(r2.height);
+} else {
+  console.log('undefined');
+}
 
 const r3 = new Rectangle(2);
-printRectangle(r3);
+console.log(r3);
+
+if (r3.width !== undefined && r3.height !== undefined) {
+  console.log(r3.width);
+  console.log(r3.height);
+} else {
+  console.log('undefined');
+}
 
 const r4 = new Rectangle(2, 0);
-printRectangle(r4);
+console.log(r4);
+
+if (r4.width !== undefined && r4.height !== undefined) {
+  console.log(r4.width);
+  console.log(r4.height);
+} else {
+  console.log('undefined');
+}
