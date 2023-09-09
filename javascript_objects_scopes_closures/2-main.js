466eefe1bb07ea5ef46b06166abddcac4d4e9a8c
Zamavundla <zsdouzee8@gmail.com>
2023-09-09 20:04:06 +0200

    task 2 update

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,43 +1,26 @@
-#!/ usr/bin/node
-
 const Rectangle = require('./2-rectangle');
+
+// Function to check if a rectangle is valid and print its width
+function printWidth(rect) {
+  if (rect.width !== undefined && rect.height !== undefined) {
+    console.log('Instance width:', rect.width);
+  } else {
+    console.log('Invalid rectangle.');
+  }
+}
 
 const r1 = new Rectangle(2, 3);
 console.log(r1);
-
-if (r1.width !== undefined && r1.height !== undefined) {
-  console.log(r1.width);
-  console.log(r1.height);
-} else {
-  console.log('Invalid rectangle.');
-}
+printWidth(r1);
 
 const r2 = new Rectangle(2, -3);
 console.log(r2);
+printWidth(r2);
 
-if (r2.width !== undefined && r2.height !== undefined) {
-  console.log(r2.width);
-  console.log(r2.height);
-} else {
-  console.log('Invalid rectangle.');
-}
-
-const r3 = new Rectangle(2);
+const r3 = new Rectangle(2, 3);
 console.log(r3);
-
-if (r3.width !== undefined && r3.height !== undefined) {
-  console.log(r3.width);
-  console.log(r3.height);
-} else {
-  console.log('Invalid rectangle.');
-}
+printWidth(r3);
 
 const r4 = new Rectangle(2, 0);
 console.log(r4);
-
-if (r4.width !== undefined && r4.height !== undefined) {
-  console.log(r4.width);
-  console.log(r4.height);
-} else {
-  console.log('Invalid rectangle.');
-}
+printWidth(r4);
