6ef03155c7265a31e53982589feac3c9636dc01b
Zamavundla <zsdouzee8@gmail.com>
2023-09-09 20:07:22 +0200

    .

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,26 +1,23 @@
 const Rectangle = require('./2-rectangle');
 
 // Function to check if a rectangle is valid and print its width
-function printWidth(rect) {
-  if (rect.width !== undefined && rect.height !== undefined) {
-    console.log('Instance width:', rect.width);
-  } else {
-    console.log('Invalid rectangle.');
-  }
-}
 
 const r1 = new Rectangle(2, 3);
 console.log(r1);
-printWidth(r1);
+console.log(r1.width);
+console.log(r1.height);
 
 const r2 = new Rectangle(2, -3);
 console.log(r2);
-printWidth(r2);
+console.log(r2.width);
+console.log(r2.height);
 
-const r3 = new Rectangle(2, 3);
+const r3 = new Rectangle(2);
 console.log(r3);
-printWidth(r3);
+console.log(r3.width);
+console.log(r3.height);
 
 const r4 = new Rectangle(2, 0);
 console.log(r4);
-printWidth(r4);
+console.log(r4.width);
+console.log(r4.height);
